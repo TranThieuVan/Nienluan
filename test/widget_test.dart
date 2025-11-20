d0babc736d1daab6ac65957cd94eb45e196ceab6
Tran Thieu Van <vanb2205921@student.ctu.edu.vn>
2025-11-20 13:31:42 +0700

    Hoàn thành

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ct484_project/main.dart';
+import '../../ct48401-project-TranThieuVan/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
